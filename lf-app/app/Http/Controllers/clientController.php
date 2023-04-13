1bbefa682c2a97bd0d50fd34dad87a03680a6c81
Leopoldo Bernardes <leopoldobms@gmail.com>
2023-04-13 13:27:00 -0300

    init docker-laravel

---
+++ lf-app/app/Http/Controllers/clientController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\clientRequest;
+use Illuminate\Http\Request;
+use App\Models\Client;
+
+class clientController extends Controller
+{
+    protected $client;
+
+    public function __construct()
+    {
+        $this->client = Client::all();
+    }
+
+    public function consult(){
+        return $this->client;
+    }
+
+    public function create(Request $request){
+        dd('die');
+        //return Client::created($request->all());
+    }
+}
